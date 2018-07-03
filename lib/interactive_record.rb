9c54bcf6bd97305f09155b911b86c1df3c3ac5c3
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-07-03 03:41:46 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,9 +55,9 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute_hash)
+    binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
     DB[:conn].execute(sql, attribute_hash.keys, attribute_hash.values)
-    binding.pry
   end
   
 end
