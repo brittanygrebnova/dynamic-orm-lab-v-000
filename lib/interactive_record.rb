d6d7736a8cd33a4e374aaaf864ec013f523cc8a9
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-07-03 03:59:09 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute_hash)
-    binding.pry
+    #binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys[0].to_s} = '#{attribute_hash.values[0]}'"
     DB[:conn].execute(sql)
   end
